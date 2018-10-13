211e664f9dadcc632f7bf0277e791930e52f262e
Bloon <mikkrebs0@gmail.com>
2018-10-13 18:02:35 +0200

    Added a handleSession that starts from a userInput "start"

---
+++ src/HandleSession.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class HandleSession {
+
+
+    public HandleSession(ArrayList<UserThread> users) {
+
+    }
+
+    public void start() {
+        System.out.println("joined game");
+
+    }
+}
