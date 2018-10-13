5a5c324962f52bac9d6af36b14ab497a971110d4
Bloon <mikkrebs0@gmail.com>
2018-10-13 18:02:08 +0200

    Added the UserThread from the old project github

---
+++ src/UserThread.java
@@ -0,0 +1,63 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class UserThread extends Thread {
+    private Server server;
+    private Socket socket;
+    private String userName;
+    private DataOutputStream output;
+    private DataInputStream input;
+
+
+    public UserThread(Server server, Socket socket) {
+        this.server = server;
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            input = new DataInputStream(socket.getInputStream());
+            output = new DataOutputStream(socket.getOutputStream());
+
+            userName = input.readUTF();
+            System.out.println(userName + " joined the server");
+            server.sendToAll(userName + " joined the server.", this);
+
+            boolean connect = true;
+            while (connect) {
+                String clientMessage = input.readUTF();
+                server.sendToAll(userName + ": " + clientMessage, this);
+                if(clientMessage.equalsIgnoreCase("quit")) {
+                    server.sendToAll(userName + " disconnected from the server", this);
+                    server.removeUser(this, userName);
+                    socket.close();
+                    connect = false;
+                }
+                if (clientMessage.equalsIgnoreCase("start")) {
+                    System.out.println(server.getUsers().size());
+                    startSession();
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String message) {
+        try {
+            output.writeUTF(message);
+            output.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startSession() {
+        HandleSession session = new HandleSession(server.getUsers());
+        session.start();
+    }
+}
