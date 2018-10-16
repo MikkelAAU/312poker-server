75c05e42b3463a437aa66e2a53de376101dca218
Jonas <37208805+jvalvik@users.noreply.github.com>
2018-10-16 13:48:00 +0200

    Implemented first round method and changed arrays to ArrayLists

--- src/Player.java
+++ src/Player.java
@@ -2,25 +2,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-   ArrayList<Card> hand;
 
-   public Player() {
-       this.hand = new ArrayList<Card>();
-   }
+	ArrayList<Card> hand;
 
-   public ArrayList<Card> getHand() {
-       return hand;
-   }
 
-    // Print all the cards in the player's hand.
-   public void printHand(){
-       System.out.println(hand);
-   }
+	public Player() {
+		this.hand = new ArrayList<Card>();
+		//players.add(this);
+		//System.out.println("New player: " + this);
+	}
 
-   @Override
-   public String toString() {
-       return "Player{" +
-               "hand=" + hand +
-               '}';
-   }
+	public ArrayList<Card> getHand() {
+		return hand;
+	}
+
+	// Print all the cards in the player's hand.
+	public void printHand(){
+		System.out.println(hand);
+	}
+
+	@Override
+	public String toString() {
+		return "Player{" +
+				"hand=" + hand +
+				'}';
+	}
+
 }
